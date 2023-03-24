965580c12fea4e5ade330c1f85e12532f4952e11
amiraElmergawy <amiraelmergawy@gmail.com>
2023-03-24 14:12:07 +0200

    Update UserModel.java
    Add constructor default and with arguments

--- src/main/java/iti/jets/amira/simpleexample/UserModel.java
+++ src/main/java/iti/jets/amira/simpleexample/UserModel.java
@@ -1,9 +1,13 @@
 package iti.jets.amira.simpleexample;
 
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @XmlRootElement
 public class UserModel {
     private String username;
