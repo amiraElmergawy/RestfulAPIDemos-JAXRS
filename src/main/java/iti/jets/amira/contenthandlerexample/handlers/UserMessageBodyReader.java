1f6e46762233e16a09d4b5ca182410a8b58d53be
Amira Elmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2023-03-30 14:38:56 +0200

    Update UserMessageBodyReader.java

--- src/main/java/iti/jets/amira/contenthandlerexample/handlers/UserMessageBodyReader.java
+++ src/main/java/iti/jets/amira/contenthandlerexample/handlers/UserMessageBodyReader.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -25,28 +26,40 @@ public class UserMessageBodyReader implements MessageBodyReader<List<UserModel>>
         return List.class.isAssignableFrom(type);
     }
 
+    /*
+     * [
+    {
+        "username":"amira",
+        "password":"123"
+    },
+    {
+        "username":"amira2",
+        "password":"1234"
+    }
+]
+     */
     @Override
     public List<UserModel> readFrom(Class<List<UserModel>> type, Type genericType,
             Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders,
             InputStream inputStream) throws IOException, WebApplicationException {
         List<UserModel> usersList = null;
-        // [UserModel(username=amira, password=123), UserModel(username=amira,
-        // password=123)]
+        // [UserModel(username=amira, password=123), UserModel(username=amira, password=123)]
         String listString = new String(inputStream.readAllBytes());
-        listString = listString.trim();
-        listString = listString.replace("[", ""); // UserModel(username=amira, password=123), UserModel(username=amira,
-                                                  // password=123)]
-        listString = listString.replace("]", ""); // UserModel(username=amira, password=123), UserModel(username=amira,
-                                                  // password=123)
-        String[] listValues = listString.split("),"); // ["UserModel(username=amira,
-                                                      // password=123","UserModel(username=amira, password=123)"]
+        listString = listString.replace("[", ""); // UserModel(username=amira, password=123), UserModel(username=amira, password=123)]
+        listString = listString.replace("]", ""); // UserModel(username=amira, password=123), UserModel(username=amira, password=123)
+        System.out.println("before trim:"+listString);
+        listString = listString.replaceAll(" ", "");
+        System.out.println("after trim:"+listString);
+        String[] listValues = listString.split("UserModel"); // ["(username=amira, password=123)","(username=amira, password=123)"]
+        List.of(listValues).stream().forEach((e)-> System.out.println(e));
+        ////////////////////////spaaaaaaaaaaaaaaaaaaaaaaaaaaaace
         for (var user : listValues) {
-            user = user.trim();
-            user = user.replace("UserModel(", ""); // "username=amira, password=123)"
+            if (user.equals(listValues[0])) continue;
+            user = user.replace("(", ""); // "username=amira, password=123)"
             user = user.replace(")", ""); // "username=amira, password=123"
             String[] userDetails = user.split(",");// ["username=amira" , "password=123"]
-            var username = userDetails[0].trim().replace("username=", ""); // amira
-            var password = userDetails[1].trim().replace("password=", "");
+            var username = userDetails[0].replace("username=", ""); // amira
+            var password = userDetails[1].replace("password=", ""); // password
             if (usersList == null)
                 usersList = new ArrayList<>();
             usersList.add(new UserModel(username, password));
